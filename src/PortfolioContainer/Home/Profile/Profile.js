cde15a19773a25dae08e87ab99d0de318da4751e
brittney-betzold <brittney.betzold2020@gmail.com>
2023-03-04 16:09:19 -0600

    resume update

--- src/PortfolioContainer/Home/Profile/Profile.js
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -1,49 +1,45 @@
-import React from 'react';
-import './Profile.css';
-import Icon from '@mdi/react';
-import { mdiMicrosoftVisualStudio } from '@mdi/js';
+import React from "react";
+import "./Profile.css";
+import Icon from "@mdi/react";
+import { mdiMicrosoftVisualStudio } from "@mdi/js";
 
 export default function Profile() {
-    return (
-        <div className='profile-container'>
-            <div className='profile-parent'>
-                <div className='profile-details'>
-                    <div className="profile-details-name">
-                        <span className="primary-text">
-                            Hello, Im <span className="highlighted-text">Brittney</span>
-
-                        </span>
-                    </div>
-                    <div className='profile-details-role'>
-                        <span className='primary-text'>
-
-                            <h1>
-                                Web Developer
-                                <Icon path={mdiMicrosoftVisualStudio}
-                                    size={2}
-                                    padding={9} />
-                            </h1>
-                            <span className="profile-role-tagline">
-                                Junior Web Developer for front and back end applications
-                            </span>
-                        </span>
-                    </div>
-                    <div className="profile-options">
-                        <a href="#HireMe">
-                            <button className="btn primary-btn">
-                                Hire Me
-                            </button>
-                        </a>
-                        <a href="WebDevResume.pdf"
-                            download="Brittney Betzold WebDevResume.pdf">
-                            <button className='btn highlighted-btn'>Get Resume</button>
-                        </a>
-                    </div>
-                </div>
-                <div className="profile-picture">
-                    <div className='profile-picture-background'></div>
-                </div>
-            </div>
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="profile-details-name">
+            <span className="primary-text">
+              Hello, Im <span className="highlighted-text">Brittney</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              <h1>
+                Web Developer
+                <Icon path={mdiMicrosoftVisualStudio} size={2} padding={9} />
+              </h1>
+              <span className="profile-role-tagline">
+                Junior Web Developer for front and back end applications
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <a href="#HireMe">
+              <button className="btn primary-btn">Hire Me</button>
+            </a>
+            <a
+              href="B.Betzold Resume.pdf"
+              download="Brittney Betzold B.Betzold Resume.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
         </div>
-    );
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
